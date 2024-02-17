47daa82757ad252eae50fccbdae5751a7531f8c3
rockwillck <38663064+rockwillck@users.noreply.github.com>
2024-02-16 23:40:07 -0600

    small calc mistake

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 function update() {
-    document.getElementById("ws").innerText = 1/2 * 0.00076 * (parseFloat(document.getElementById("vf").value)**2 - parseFloat(document.getElementById("v0").value)**2 + 9.8*(parseFloat(document.getElementById("hf").value) - parseFloat(document.getElementById("h0").value)))
+    document.getElementById("ws").innerText = 0.00076 * (1/2*(parseFloat(document.getElementById("vf").value)**2 - parseFloat(document.getElementById("v0").value)**2) + 9.8*(parseFloat(document.getElementById("hf").value) - parseFloat(document.getElementById("h0").value)))
 }
